1b517ee6cc9eaec4031100efd75aa331537a9181
shoyagishita <yagishita-tky@umin.ac.jp>
2025-11-15 14:30:13 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ylabcommon',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     # package_dir={"": "ylabcommon"},
     install_requires=[
