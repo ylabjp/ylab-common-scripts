57c43ea25c4d49dda5c03fad39e9c50107ef6de7
shoyagishita <yagishita-tky@umin.ac.jp>
2025-11-24 09:39:14 +0900

    Update general.py

--- ylabcommon/models/parameters/general.py
+++ ylabcommon/models/parameters/general.py
@@ -2,18 +2,18 @@ import argparse
 from pydantic import BaseModel, Field
 
 class ArgModel(BaseModel):
-    overwrite:bool = Field(False)
+    overwrite: bool = Field(False)
 
-def standard_arg_parser()->ArgModel:
+def standard_arg_parser() -> ArgModel:
     parser = argparse.ArgumentParser(description='Standard parser')
     parser.add_argument(
         '-o',
         '--overwrite',
         action='store_true',
-        # type=bool,
         default=False,
         help='Overwrite existing analysis results'
     )
     args = parser.parse_args()
-    return ArgModel(**args)
+    # print(vars(args))
+    return ArgModel(**vars(args))
 
